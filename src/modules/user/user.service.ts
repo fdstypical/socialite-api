f811cdddec3785e47247bee355d3e29ec17ed955
Andrey Shulmin <ashulmin14@gmail.com>
2022-09-18 00:17:27 +0300

    added nested includes for user->avatar->static_field

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/sequelize';
 import { ErrorMessage } from 'src/core/constants/error.messages';
 import { BadRequestException } from 'src/core/exceptions/build-in/bad-request.exception';
 import { AsyncContext } from 'src/core/modules/async-context/async-context';
-import { Interest, Role, User, UserAvatar } from 'src/models';
+import { Interest, Role, StaticField, User, UserAvatar } from 'src/models';
 import { RoleName } from 'src/types/common.types';
 import { RoleService } from '../role/role.service';
 import { UserAvatarService } from '../user-avatar/user-avatar.service';
@@ -26,7 +26,7 @@ export class UserService {
         Role,
         { model: Interest, as: 'createdInterests' },
         { model: Interest, as: 'interests' },
-        { model: UserAvatar },
+        { model: UserAvatar, include: [StaticField] },
       ],
     });
   }
