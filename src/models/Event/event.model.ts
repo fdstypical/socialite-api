07da81226b794fe1a102e29ac9cad4a4d6fd7550
Andrey Shulmin <ashulmin14@gmail.com>
2022-11-17 23:52:13 +0300

    added events model

--- src/models/Event/event.model.ts
+++ src/models/Event/event.model.ts
@@ -1,5 +1,45 @@
-import { Model, Table } from 'sequelize-typescript';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 import { CreateEventAttributes } from './interfaces';
 
 @Table({ tableName: 'events' })
-export class Event extends Model<Event, CreateEventAttributes> {}
+export class Event extends Model<Event, CreateEventAttributes> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  readonly id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  readonly title: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  readonly description: string;
+
+  @Column({
+    type: DataType.DATE,
+    defaultValue: DataType.NOW,
+    allowNull: false,
+  })
+  readonly startDate: Date;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: false,
+    allowNull: false,
+  })
+  readonly isPrivate: boolean = false;
+
+  @Column({
+    type: DataType.DATE,
+    allowNull: true,
+  })
+  readonly expiredDate: Date | null;
+}
